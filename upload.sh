47df648a69bbe9f744e3b302bf9e65e7550e0208
tanuj108 <71034587+tanuj108@users.noreply.github.com>
2020-10-11 14:17:42 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,4 +2,4 @@
 
 echo "hi, we are in upload.sh.......this script will upload the specified file/directory in the s3bucket"
 
-/usr/local/bin/s3cmd sync --delete-removed "$DATA_PATH" "$S3_PATH"
+/usr/local/bin/s3cmd sync"$DATA_PATH" "$S3_PATH"
